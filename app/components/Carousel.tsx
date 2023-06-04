01605cc458da44e0b09227961d802b9900df42d6
dimakuznetsow <108948435+dimakuznetsow@users.noreply.github.com>
2023-06-04 18:07:49 +0300

    carousel images changes

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -5,37 +5,41 @@ import Link from "next/link"
 
 
 function Carousel() {
+
+
     return (
         <div className="carousel  mb-10 mx-4 lg:mx-48">
             <div id="slide1" className="carousel-item relative w-full">
-                <img src="https://ecommerce-aleph.s3.eu-west-1.amazonaws.com/Delivery.png" className="w-full" />
+                <img src="https://ecommerce-aleph.s3.eu-west-1.amazonaws.com/Delivery.png" className="w-full hidden lg:block" />
+                <img src="https://ecommerce-aleph.s3.eu-west-1.amazonaws.com/Delivery-xs.png" className="w-full block lg:hidden" />
+
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide4" className="btn btn-circle">❮</a>
+                    <a href="" className=""></a>
                     <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
 
             <div id="slide2" className="carousel-item relative w-full">
-                <img src={chocolates.src} className="w-full" />
+
+                <img src="https://ecommerce-aleph.s3.eu-west-1.amazonaws.com/Bissli.jpg" className="w-full hidden lg:block" />
+                <img src="https://ecommerce-aleph.s3.eu-west-1.amazonaws.com/Bissli-xs.png" className="w-full block lg:hidden" />
+
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide1" className="btn btn-circle">❮</a>
                     <a href="#slide3" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide3" className="carousel-item relative w-full">
-                <img src={teaAndCoffee.src} className="w-full" />
+                <img src="https://ecommerce-aleph.s3.eu-west-1.amazonaws.com/Bamba.png" className="w-full hidden lg:block" />
+                <img src="https://ecommerce-aleph.s3.eu-west-1.amazonaws.com/Bamba-xs.png" className="w-full block lg:hidden" />
+
+
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide2" className="btn btn-circle">❮</a>
-                    <a href="#slide4" className="btn btn-circle">❯</a>
+
                 </div>
             </div>
-            <div id="slide4" className="carousel-item relative w-full">
-                <img src={snacks.src} className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide3" className="btn btn-circle">❮</a>
-                    <a href="#slide1" className="btn btn-circle">❯</a>
-                </div>
-            </div>
+
         </div>
     )
 }
